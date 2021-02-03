f3eb451307b6fa8822ee5433357aec2291f611e0
Kendrick Tan <kendricktan0814@gmail.com>
2021-02-04 09:02:26 +1000

    readme

--- README.md
+++ README.md
@@ -2,15 +2,18 @@
 
 MVP to demonstrate Slither bug when composing library calls in a very specific format.
 
+`contracts/Example:Example.sol` depicts the two functions, `fails`, and `works`, both with a similar logical statement. However `fails` fails to get parsed by `slither` and `works` is successfully parsed by slither.
+
 ```bash
 # slither --version
 # 0.7.0
 
 slither .
 
-ERROR:root:Error:
-ERROR:root:Function not found on TMP_22(None) = HIGH_LEVEL_CALL, dest:TMP_21(None), function:us_mul, arguments:['_lowerAlpha']  . Please try compiling with a recent Solidity version. 'NoneType' object has no attribute 'type
+> ERROR:root:Error:
+> ERROR:root:Function not found on TMP_22(None) = HIGH_LEVEL_CALL, dest:TMP_21(None), function:us_mul, arguments:['_lowerAlpha']  . Please try compiling with a recent Solidity version. 'NoneType' object has no attribute 'type
 ```
+
 
 ## Related Issues
 
--- contracts/Example.sol
+++ contracts/Example.sol
@@ -52,4 +52,45 @@ contract Example {
             }
         }
     }
+
+    function works(
+        int128 _alpha,
+        int128 _oGLiq,
+        int128 _nGLiq,
+        int128[] memory _oBals,
+        int128[] memory _nBals,
+        int128[] memory _weights
+    ) private pure {
+        uint256 _length = _nBals.length;
+
+        for (uint256 i = 0; i < _length; i++) {
+            int128 _nIdeal = _nGLiq.us_mul(_weights[i]);
+
+            if (_nBals[i] > _nIdeal) {
+                int128 _upperAlpha = ONE + _alpha;
+
+                int128 _nHalt = _nIdeal.us_mul(_upperAlpha);
+
+                if (_nBals[i] > _nHalt) {
+                    int128 _oHalt = _oGLiq.us_mul(_weights[i]).us_mul(_upperAlpha);
+
+                    if (_oBals[i] < _oHalt) revert("upper-halt");
+                    if (_nBals[i] - _nHalt > _oBals[i] - _oHalt) revert("upper-halt");
+                }
+            } else {
+                int128 _lowerAlpha = ONE - _alpha;
+
+                int128 _nHalt = _nIdeal.us_mul(_lowerAlpha);
+
+                if (_nBals[i] < _nHalt) {
+                    // Only thing changed
+                    int128 _oHalt = _oGLiq.us_mul(_weights[i]);
+                    _oHalt = _oHalt.us_mul(_lowerAlpha);
+
+                    if (_oBals[i] > _oHalt) revert("lower-halt");
+                    if (_nHalt - _nBals[i] > _oHalt - _oBals[i]) revert("lower-halt");
+                }
+            }
+        }
+    }
 }
